b2a46a9a43f0817fdc68d088d619c96950b7b15d
Andrew <15279886+Warrenera@users.noreply.github.com>
2020-11-26 13:54:59 -0500

    Refactored to be object-oriented, replaced custom text file with CSV and INI files.

--- events.py
+++ events.py
@@ -1,136 +1,124 @@
-# TODO: Look into using actual data structure like JSON instead of custom "x : y" text formatting
 # TODO: Look into official commenting etiquette for good comment hygiene
 
-from calendar import setfirstweekday, monthcalendar
+import csv
+import configparser
 from datetime import datetime
+from calendar import setfirstweekday, monthcalendar
 
 
-def get_current_list():
-    """
-    Read events from text file; gets last known year, Mother's, and Father's day
-
-    :return: dict of important events
-    :return: last known year the program was run
-    :return: Mother's Day this year
-    :return: Father's Day this year
-    """
-    events = {}
-    with open("events.txt", "r") as file:
-        for line in file:
-            if line.startswith("0") or line.startswith("1"):
-                (key, val) = line.split(" : ")
-                events[key] = val.rstrip("\n")
-            elif line.startswith("y"):
-                last_year_ran = int(line[4:])
-            elif line.startswith("f"):
-                last_fathers_day = line[4:].rstrip("\n")
-            elif line.startswith("m"):
-                last_mothers_day = line[4:].rstrip("\n")
-    return events.items(), last_year_ran, last_fathers_day, last_mothers_day
-
-
-def get_varying_days(event_items, last_mothers_day, last_fathers_day, current_year):
-    next_mothers_day = "05-"
-    next_fathers_day = "06-"
-    month = 5    # Mother's Day is always in May, the fifth month
+class CurrentYear:
+    mothers_day = "05-"
+    fathers_day = "06-"
+    month = 5  # Mother's Day is always in May, the fifth month
     sundays = 2  # Mother's Day is always the second Sunday in May
 
-    for i in range(0, 2):
-        if i == 0:
-            next_mothers_day += get_day(current_year, month, sundays)
+    def __init__(self):
+        self.now = datetime.now()
+        self.today = self.now.strftime("%m-%d")
+        self.this_year = int(self.now.strftime("%Y"))
+
+        self.config = configparser.ConfigParser()
+        self.last_year_ran, self.last_fathers_day, self.last_mothers_day = self.get_last_varying_values(self.config)
+        self.event_items = self.get_events()
+
+    def get_last_varying_values(self, config):
+        varying_values = ["last_year_ran", "last_fathers_day", "last_mothers_day"]
+        config.read("varying_values.ini")
+        for i, var in enumerate(varying_values):
+            varying_values[i] = self.config.get("last_known_values", var)
+        return [varying_values[0], varying_values[1], varying_values[2]]
+
+    def get_events(self):
+        events = {self.last_fathers_day: "Father's Day", self.last_mothers_day: "Mother's Day"}
+        with open("events.csv", "r") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                events[row["date"]] = row["event"].rstrip("\n")
+        return events.items()
+
+    def set_new_varying_values(self):
+        for i in range(0, 2):
+            if i == 0:
+                self.mothers_day += self.get_day()
+            else:
+                self.month += 1    # Father's Day is always in June, the sixth month
+                self.sundays += 1  # Father's Day is always the third Sunday in June
+                self.fathers_day += self.get_day()
+        with open("varying_values.ini", "w") as conf:
+            self.config["last_known_values"]["last_mothers_day"] = self.mothers_day
+            self.config["last_known_values"]["last_fathers_day"] = self.fathers_day
+            self.config["last_known_values"]["last_year_ran"] = str(self.this_year)
+            self.config.write(conf)
+
+    def get_day(self):
+        i = 0
+        setfirstweekday(6)  # Sets the first day of the week to Sunday
+        for week in monthcalendar(self.this_year, self.month):
+            if week[0] == 0:
+                continue  # If the first day of the month is not Sunday, disregard that week
+            else:
+                i += 1
+                if i == self.sundays:
+                    return str(week[0])
+
+
+class EventReminder:
+    def __init__(self, current_year):
+        self.current_year = current_year
+        self.this_month = []
+        self.this_week = []
+        self.this_day = []
+        self.this_month, self.this_week, self.this_day = self.append_events()
+
+    def append_events(self):
+        for date, event in self.current_year.event_items:
+            d = datetime.strptime(date, "%m-%d")
+            t = datetime.strptime(self.current_year.today, "%m-%d")
+
+            if 7 < (d - t).days <= 31:
+                self.this_month.append((date, event))
+            if 1 < (d - t).days <= 7:
+                self.this_week.append((date, event))
+            if date == self.current_year.today:
+                self.this_day.append((date, event))
+        return [self.this_month, self.this_week, self.this_day]
+
+    def print_events(self):
+        print("Hello! Welcome to your family and friend birthday and important event reminder, brought to you by wonderful Python!")
+
+        if not self.this_month and not self.this_week and not self.this_day:
+            print("It looks like there aren't any events coming up!")
         else:
-            month += 1    # Father's Day is always in June, the sixth month
-            sundays += 1  # Father's Day is always the third Sunday in June
-            next_fathers_day += get_day(current_year, month, sundays)
-
-    with open("events.txt", "w") as file:
-        file.write("m : " + next_mothers_day + "\n")
-        file.write("f : " + next_fathers_day + "\n")
-        file.write("y : " + str(current_year) + "\n")
-        for date, event in event_items:
-            if date == last_mothers_day:
-                date = next_mothers_day
-            elif date == last_fathers_day:
-                date = next_fathers_day
-            file.write(date + " : " + event + "\n")
-
-
-def get_day(current_year, month, sundays):
-    i = 0
-    setfirstweekday(6)  # Sets the first day of the week to Sunday
-    for week in monthcalendar(current_year, month):
-        if week[0] == 0:
-            continue  # If the first day of the month is not Sunday, disregard that week
-        else:
-            i += 1
-            if i == sundays:
-                return str(week[0])
-
-
-def append_events(event_items, today, this_month, this_week, this_day):
-    for date, event in event_items:
-        d = datetime.strptime(date, "%m-%d")
-        t = datetime.strptime(today, "%m-%d")
-
-        if 0 <= (d - t).days <= 31:
-            this_month.append((date, event))
-        if 0 <= (d - t).days <= 7:
-            this_week.append((date, event))
-        if date == today:
-            this_day.append((date, event))
-    return [this_month, this_week, this_day]
-
-
-def print_events(this_month, this_week, this_day):
-    print("Hello! Welcome to your family and friend birthday and important event reminder, brought to you by wonderful Python!")
-
-    if not this_month and not this_week and not this_day:
-        print("It looks like there aren't any events coming up!")
-    else:
-        if this_month:
-            print("Just a heads up, the following events are occurring in the next thirty days:\n")
-            for event in this_month:
-                print("     >{0} : {1}".format(*event))
-            print("\nI recommend you start looking for a gift!\n")
-
-        if this_week:
-            print("~*~*~*~*~\n\nThe following events are occurring in the next seven days:\n")
-            for event in this_week:
-                print("     >{0} : {1}".format(*event))
-            print("\nIf you haven't picked up a gift yet, you need to now!\n")
-
-        if this_day:
-            print("~*~*~*~*~\n\nThe following events are occurring today:\n")
-            for event in this_day:
-                print("     >{0} : {1}".format(*event))
-            print("\nCall them sometime today!")
+            if self.this_month:
+                print("Just a heads up, the following events are occurring in the next month:\n")
+                for event in self.this_month:
+                    print("     >{0} : {1}".format(*event))
+                print("\nI recommend you start looking for a gift!\n")
+            if self.this_week:
+                print("~*~*~*~*~\n\nThe following events are occurring in the next week:\n")
+                for event in self.this_week:
+                    print("     >{0} : {1}".format(*event))
+                print("\nIf you haven't picked up a gift yet, you need to now!\n")
+            if self.this_day:
+                print("~*~*~*~*~\n\nThe following events are occurring today:\n")
+                for event in self.this_day:
+                    print("     >{0} : {1}".format(*event))
+                print("\nCall them sometime today!")
 
 
 def main():
-    this_month = []
-    this_week = []
-    this_day = []
-
-    now = datetime.now()
-    today = now.strftime("%m-%d")
-    current_year = int(now.strftime("%Y"))
-
-    event_items, last_year_ran, last_fathers_day, last_mothers_day = get_current_list()
+    current_year = CurrentYear()
+    event_reminder = EventReminder(current_year)
 
     # Gets the dates for this year's Mother's and Father's Days
     # 6-11-18: Logic error where new dates would never be fetched may occur if file year is correct but the dates aren't
     # Given this should only happen if dates were manually changed, it was deemed too unlikely to occur to bother fixing
-    if last_year_ran != current_year:
-        get_varying_days(event_items, last_mothers_day, last_fathers_day, current_year)
+    if current_year.last_year_ran != current_year.this_year:
+        current_year.set_new_varying_values()
 
-    this_month, this_week, this_day = append_events(event_items, today, this_month, this_week, this_day)
-
-    print_events(this_month, this_week, this_day)
+    event_reminder.print_events()
 
 
 # Initializes main function
 if __name__ == "__main__":
     main()
-
-
-# Hello there
