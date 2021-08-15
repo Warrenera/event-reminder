e0175a8a992b51ec172bfd240ac8450f7d8733f5
Andrew <15279886+warrenera@users.noreply.github.com>
2021-08-15 04:04:12 -0400

    Updated default batch file filepath

--- events.bat
+++ events.bat
@@ -1,5 +1,5 @@
 @echo off
 REM Add this to the startup folder on Windows 10 for the script to run on machine startup
 REM Default Windows 10 startup folder location: C:\Users\[User Name]\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
-python C:\Python\Projects\Events\events.py
+python C:\Repositories\event-reminder\events.py
 pause
